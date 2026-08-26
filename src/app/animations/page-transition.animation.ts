be52328a21ae458a7620377296e7480ea669495a
FREYSS <you@example.com>
2026-08-26 16:44:43 +0200

    feat: correction sens animation transition page

--- src/app/animations/page-transition.animation.ts
+++ src/app/animations/page-transition.animation.ts
@@ -1,26 +1,40 @@
-import { AnimationController } from '@ionic/angular';
+import { Animation, AnimationController } from '@ionic/angular';
 
 export const pageTransition = (baseEl: HTMLElement, opts?: any) => {
     const enteringEl = opts.enteringEl;
     const leavingEl = opts.leavingEl;
-
     const animCtrl = new AnimationController();
+    let enteringAnimation: Animation;
+    let leavingAnimation: Animation;
 
-    const enterAnim = animCtrl
-        .create()
-        .addElement(enteringEl)
-        .fromTo('transform', 'translateX(100%)', 'translateX(0)')
-        .fromTo('opacity', '0.5', '1');
+    if(opts.direction == "forward"){
+        leavingAnimation = animCtrl
+            .create()
+            .addElement(leavingEl)
+            .fromTo('transform', 'translateX(0)', 'translateX(-100%)')
+            .fromTo('opacity', '1', '0');
 
-    const leaveAnim = animCtrl
-        .create()
-        .addElement(leavingEl)
-        .fromTo('transform', 'translateX(0)', 'translateX(-100%)')
-        .fromTo('opacity', '1', '0.5');
+        enteringAnimation = animCtrl
+            .create()
+            .addElement(enteringEl)
+            .fromTo('transform', 'translateX(100%)', 'translateX(0)')
+            .fromTo('opacity', '0', '1');
+    }else{
+        leavingAnimation = animCtrl
+            .create()
+            .addElement(leavingEl)
+            .fromTo('transform', 'translateX(0)', 'translateX(100%)')
+            .fromTo('opacity', '1', '0');
 
+        enteringAnimation = animCtrl
+            .create()
+            .addElement(enteringEl)
+            .fromTo('transform', 'translateX(-100%)', 'translateX(0)')
+            .fromTo('opacity', '0', '1');
+    }
     return animCtrl
         .create()
-        .duration(300)
+        .duration(500)
         .easing('ease-in-out')
-        .addAnimation([enterAnim, leaveAnim]);
+        .addAnimation([leavingAnimation, enteringAnimation]);
 };
